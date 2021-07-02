92fc5264d98b9cbc3c5ce6a8e4aab0cecce970ef
sotamain <sotamain@LAPTOP-FT2PTUO1>
2021-07-02 11:23:44 +0900

    変更しました

--- gittest/src/gittest/MainYunoki.java
+++ gittest/src/gittest/MainYunoki.java
@@ -5,7 +5,9 @@ public class MainYunoki {
 	public static void main(String[] args) {
 
 		System.out.print("柚木颯太");
-		System.out.print("快速 海里　酒田行");
+		System.out.print("ようこそ");
+		System.out.print("WELCOME");
+		System.out.print("test");
 
 	}
 
